dca8823711e8fc6856625299817f70cfc2971927
Desu1243 <lubonskimateusz@gmail.com>
2023-05-13 17:17:13 +0200

    project init

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.light(),
+    home: const Scaffold(
+      body: Text("Nothing"),
+    ),
+  ));
+}
