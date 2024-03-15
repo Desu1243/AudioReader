f4be17e043ed296c9486ec1f1cceeb44c11776da
Desu1243 <lubonskimateusz@gmail.com>
2024-03-15 15:55:36 +0100

    let's start over

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-import 'package:audioreader/pages/LoadingPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: LoadingPage()
+      home: Scaffold()
     )
   );
 }
